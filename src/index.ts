a56424915a96dd7e29627bd37319fdfb0d694ebf
Luiz Felipe <felipe.pinghang2@gmail.com>
2026-05-23 20:33:16 -0300

    fix: :bug: [#1] changing the logic from environment checking to log recording

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import app from "./app"
 
-const port = 3000
+const port = Number(process.env.PORT) || 3001
 
 app.listen(port, '0.0.0.0', () => {
     console.log(`✅ | Servidor rodando na porta http://0.0.0.0:${port}`)
--- src/middleware/requestLogger.middleware.ts
+++ src/middleware/requestLogger.middleware.ts
@@ -2,29 +2,29 @@ import { Request, Response, NextFunction } from "express";
 import readFile from "../utils/readFile";
 import createFile from "../utils/createFile";
 
-async function createLogger(log: {date: string, method: string, url: string, status: number, duration: string}) {
+async function createLogger(log: { date: string, method: string, url: string, status: number, duration: string }) {
     const path = "request.log"
     const content = readFile(path) + JSON.stringify(log) + "\n"
     createFile(path, content)
 }
 
 function requestLoggerMiddleware(req: Request, res: Response, next: NextFunction) {
-    if (process.env.NODE_ENV !== "development") next()
+    if (process.env.NODE_ENV === "development") {
+        const start = Date.now();
 
-    const start = Date.now();
-
-    res.on("finish", () => {
-        const duration = Date.now() - start;
-        const now = new Date()
-        const log = {
-            date: now.toLocaleString("pt-br"),
-            method: req.method,
-            url: req.originalUrl,
-            status: res.statusCode,
-            duration: `${duration}ms`
-        }
-        createLogger(log)
-    });
+        res.on("finish", () => {
+            const duration = Date.now() - start;
+            const now = new Date()
+            const log = {
+                date: now.toLocaleString("pt-br"),
+                method: req.method,
+                url: req.originalUrl,
+                status: res.statusCode,
+                duration: `${duration}ms`
+            }
+            createLogger(log)
+        });
+    }
 
     next();
 }
