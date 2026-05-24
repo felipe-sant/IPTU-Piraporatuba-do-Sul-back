29c652b571da855bce4bbc9a3c220a65929ee1eb
Luiz Felipe <felipe.pinghang2@gmail.com>
2026-05-24 12:26:57 -0300

    docs: :bulb: [no-task] changing the .env.example file and modifying the index execution log

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,7 @@ import app from "./app"
 const port = Number(process.env.PORT) || 3001
 
 app.listen(port, '0.0.0.0', () => {
-    console.log(`✅ | Servidor rodando na porta http://0.0.0.0:${port}`)
+    console.log(`✅ | Servidor em execução, porta interna ${port}!`)
+}).on('error', (err) => {
+    console.error(`❌ | Erro ao iniciar o servidor: ${err}`)
 })
